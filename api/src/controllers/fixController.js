dd6dce77f63cc89d439d0ae117e67560cc84c401
faculpez <faculpez1998@gmail.com>
2023-02-19 22:11:27 -0300

    controllers modified

--- api/src/controllers/fixController.js
+++ api/src/controllers/fixController.js
@@ -14,13 +14,13 @@ export const createFix = async(req,res) =>{
     try {
         const { phoneId, description} = req.body;
         if( !phoneId || !description){
-            res.status(400).json({message:'Incomplete data'});
+            return res.status(400).json({message:'Incomplete data'});
         }
         const status = 'pending'  //STATUS DEFAULT
         const newFix = await Fix.create({phoneId, description, status})
-        res.status(200).json({message: 'created fix succesful'})
+        return res.status(200).json({message: 'created fix succesful'})
     } catch (error) {
-        res.status(500).json({message: error.message})
+        return res.status(500).json({message: error.message})
     }
 }
 
